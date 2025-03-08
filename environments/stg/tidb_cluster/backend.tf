20c1d92f38cc09f55a504ffc1fa4340c4470a29d
bbrfkr <bbrfkr@gmail.com>
2025-03-08 12:10:07 +0000

    mod

--- environments/stg/tidb_cluster/backend.tf
+++ environments/stg/tidb_cluster/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "mycloud-tfstates"
-    key    = "stg/mysql_cluster"
+    key    = "stg/tidb_cluster"
     endpoints = {
       s3 = "https://swift.dynamis.bbrfkr.net"
     }
---
+++ environments/stg/tidb_cluster/main.tf
@@ -0,0 +1,12 @@
+module "tidb_cluster" {
+  source           = "../../../modules/tidb_cluster"
+  environment_name = data.terraform_remote_state.common.outputs.environment_name
+  project_id       = data.terraform_remote_state.global_common.outputs.project_id
+  image_id         = var.image_id
+  flavor_id        = var.flavor_id
+  network_id       = data.terraform_remote_state.networking.outputs.all.network_id
+  bastion_sg_id    = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
+  subnet_cidr      = data.terraform_remote_state.networking.outputs.all.subnet_cidr
+  subnet_id        = data.terraform_remote_state.networking.outputs.all.subnet_id
+  external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
+}
