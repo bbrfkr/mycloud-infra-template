5e04d3ecf9c3c3a6984848e745764615ce746ae0
bbrfkr <bbrfkr@gmail.com>
2025-08-28 14:08:49 +0000

    support rtx50xx

--- environments/prod/ai_servers/main.tf
+++ environments/prod/ai_servers/main.tf
@@ -1,13 +1,14 @@
 locals {
   completion_gpu_count = 1
   additional_gpu_count = 1
-  vllm_gpu_count       = 2
-  stable_vllm_image_id = "e71eaca8-46b8-4088-92f1-199c8b1c1f0a"
-  latest_vllm_image_id = "970321bf-7e39-4512-8488-6565736e7e24"
+  vllm_gpu_count       = 4
+  stable_vllm_image_id = "6c8991d8-8be0-4b03-8fbe-fd2364f3ae2d"
+  latest_vllm_image_id = "52f80fb5-519f-4b80-9988-b735ea032687"
   comfyui_gpu_count    = 2
-  comfyui_image_id     = "7e294288-50a0-47ce-be5a-c8bc5995fc43"
+  comfyui_image_id     = "321692b6-c646-4c5e-9925-5740d88e8cfc"
   p2_xlarge_id         = "4840358c-4b90-4d54-a97e-f321a646ee6b"
   p2_2xlarge_id        = "85c71275-8bdf-48a1-8d40-699b34f4f7dc"
+  p2_3xlarge_id        = "981636a2-41bc-4e98-9582-b4dba1cab03d"
   p2_4xlarge_id        = "b90287c6-a704-4e8b-983e-7c8133ac56c3"
   p3_2xlarge_id        = "5e27e863-efbf-4ccd-8271-9840eab638e4"
   p3_4xlarge_id        = "c3ab8bf5-ac6c-4d66-8f5e-9e519ba01cc9"
@@ -43,13 +44,13 @@ module "additional" {
   huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
   models_config = [
     {
-      model_name           = "nomic-ai/nomic-embed-text-v1.5"
-      vllm_command_args = "--served-model-name bbrfkr-embedding --gpu-memory-utilization 0.5 --task embedding --trust-remote-code --max-model-len 8192"
+      model_name           = "BAAI/bge-base-en-v1.5"
+      vllm_command_args = "--served-model-name bbrfkr-embedding --gpu-memory-utilization 0.45 --task embed --trust-remote-code --max-model-len 8192"
       port = 8000
     },
     {
       model_name           = "Qwen/Qwen3-Reranker-0.6B"
-      vllm_command_args = "--served-model-name bbrfkr-reranker --gpu-memory-utilization 0.5 --task score --max-model-len 8192"
+      vllm_command_args = "--served-model-name bbrfkr-reranker --gpu-memory-utilization 0.45 --task score --max-model-len 8192"
       port = 8001
     },
   ]
@@ -75,15 +76,14 @@ module "vllm_1" {
   network_id           = data.terraform_remote_state.networking.outputs.all.network_id
   bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
   external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
-  flavor_id            = local.p3_2xlarge_id
+  flavor_id            = local.p3_4xlarge_id
   image_id             = local.stable_vllm_image_id
   resource_suffix      = "1"
   gpu_count            = local.vllm_gpu_count
   gpu_power_limit      = 250
   model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
-  # original qwen3-coder definition
+  vllm_command_args    = "--served-model-name bbrfkr-llm --tensor-parallel-size ${local.vllm_gpu_count} --max-model-len 228000 --enable-expert-parallel --max-num-seqs 1 --gpu-memory-utilization 0.85"
   # vllm_command_args    = "--served-model-name bbrfkr-llm --tensor-parallel-size ${local.vllm_gpu_count} --max-model-len 155648 --max-num-seqs 1 --gpu-memory-utilization 0.85 --enable-expert-parallel --tool-call-parser qwen3_xml --tool-parser-plugin /home/ubuntu/.cache/huggingface/hub/models--Qwen--Qwen3-Coder-30B-A3B-Instruct-FP8/snapshots/b771c46af0ca185345753e6f973a4ce2b74205f0/qwen3coder_tool_parser.py --enable-auto-tool-choice"
-  vllm_command_args    = "--invalid-option"
   huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
 }
 
@@ -93,44 +93,44 @@ module "vllm_2" {
   network_id           = data.terraform_remote_state.networking.outputs.all.network_id
   bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
   external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
-  flavor_id            = local.p3_2xlarge_id
-  image_id             = local.stable_vllm_image_id
+  flavor_id            = local.p2_4xlarge_id
+  image_id             = local.latest_vllm_image_id
   resource_suffix      = "2"
   gpu_count            = local.vllm_gpu_count
-  gpu_power_limit      = 250
-  model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
-  vllm_command_args    = "--invalid-option"
+  gpu_power_limit      = 180
+  model_name           = "openai/gpt-oss-20b"
+  vllm_command_args    = "--served-model-name bbrfkr-gpt --tensor-parallel-size ${local.vllm_gpu_count} --max-num-seqs 8 --gpu-memory-utilization 0.85"
   huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
 }
 
-module "vllm_3" {
-  source               = "../../../modules/vllm"
-  environment_name     = data.terraform_remote_state.common.outputs.environment_name
-  network_id           = data.terraform_remote_state.networking.outputs.all.network_id
-  bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
-  external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
-  flavor_id            = local.p2_2xlarge_id
-  image_id             = local.latest_vllm_image_id
-  resource_suffix      = "3"
-  gpu_count            = local.vllm_gpu_count
-  gpu_power_limit      = 250
-  model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
-  vllm_command_args    = "--invalid-option"
-  huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
-}
+# module "vllm_3" {
+#   source               = "../../../modules/vllm"
+#   environment_name     = data.terraform_remote_state.common.outputs.environment_name
+#   network_id           = data.terraform_remote_state.networking.outputs.all.network_id
+#   bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
+#   external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
+#   flavor_id            = local.p2_xlarge_id
+#   image_id             = local.latest_vllm_image_id
+#   resource_suffix      = "3"
+#   gpu_count            = local.vllm_gpu_count
+#   gpu_power_limit      = 250
+#   model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
+#   vllm_command_args    = "--served-model-name bbrfkr-llm --tensor-parallel-size ${local.vllm_gpu_count} --max-num-seqs 1 --gpu-memory-utilization 0.85"
+#   huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
+# }
 
-module "vllm_4" {
-  source               = "../../../modules/vllm"
-  environment_name     = data.terraform_remote_state.common.outputs.environment_name
-  network_id           = data.terraform_remote_state.networking.outputs.all.network_id
-  bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
-  external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
-  flavor_id            = local.p2_2xlarge_id
-  image_id             = local.latest_vllm_image_id
-  resource_suffix      = "4"
-  gpu_count            = local.vllm_gpu_count
-  gpu_power_limit      = 250
-  model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
-  vllm_command_args    = "--invalid-option"
-  huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
-}
+# module "vllm_4" {
+#   source               = "../../../modules/vllm"
+#   environment_name     = data.terraform_remote_state.common.outputs.environment_name
+#   network_id           = data.terraform_remote_state.networking.outputs.all.network_id
+#   bastion_sg_id        = data.terraform_remote_state.bastion.outputs.all.bastion_sg_id
+#   external_subnet_name = data.terraform_remote_state.global_common.outputs.external_subnet_name
+#   flavor_id            = local.p2_2xlarge_id
+#   image_id             = local.latest_vllm_image_id
+#   resource_suffix      = "4"
+#   gpu_count            = local.vllm_gpu_count
+#   gpu_power_limit      = 250
+#   model_name           = "Qwen/Qwen3-Coder-30B-A3B-Instruct-FP8"
+#   vllm_command_args    = "--invalid-option"
+#   huggingface_hf_token = base64decode(data.openstack_keymanager_secret_v1.huggingface_hf_token.payload)
+# }
