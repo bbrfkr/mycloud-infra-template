e68fbefb01fa15177421970d5dfdf3b7c5d0971e
bbrfkr <bbrfkr@gmail.com>
2025-12-14 11:05:09 +0000

    update for mistral-common

--- modules/vllm/main.tf
+++ modules/vllm/main.tf
@@ -138,7 +138,7 @@ echo "aimodel-nfs.home.dynamis.bbrfkr.net:$${share_point} $${mount_point} nfs de
 mount -a
 
 # hotfix mistral tool call
-pip install -U "git+https://github.com/bbrfkr/mistral-common@main"
+/home/ubuntu/.pyenv/shims/pip install -U "git+https://github.com/bbrfkr/mistral-common@main"
 
 # configure vllm
 cat <<EOF > /etc/systemd/system/vllm.service
